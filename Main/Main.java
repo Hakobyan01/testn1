671399ce07abcbf51400a3e0122bc81710bdb247
xach1234567890 <xach.hakobyan.11@mail.ru>
2020-03-13 20:16:15 +0400

    #aaaaaaaaaaaaaaa

--- Main/Main.java
+++ Main/Main.java
@@ -5,6 +5,8 @@ public class Main
         System.out.println("aaaaaaaaaaaaaa");
         System.out.println("aaaaaaaaaaaaaa");
         System.out.println("aaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaa11111122222222");
 
     }
 }
